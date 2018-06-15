22deab3251d9945c7101248cda3f839c2d5f1464
Mosa Al-Husseini <mosaalhusseini@gmail.com>
2018-06-15 10:02:06 +0200

    [TASK] add option to modify CKeditor styles/headers

---
+++ ext_localconf.php
@@ -0,0 +1,7 @@
+<?php
+if (!defined('TYPO3_MODE')) {
+    die('Access denied.');
+}
+
+$GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['subtheme_t3kit_template_default']
+= 'EXT:subtheme_t3kit_template/Resources/Private/Extensions/RTE/Default.yaml';
