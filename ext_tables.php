0e84719fd5b44a0cb7d22dee8347dde7cc41cad8
dmh <dmytro.hrynevych@gmail.com>
2018-10-29 14:21:38 +0200

    [TASK] register icons outside BE if-statement so it can show content element icons in frontend editing

--- ext_tables.php
+++ ext_tables.php
@@ -2,23 +2,20 @@
 defined('TYPO3_MODE') or die();
 
 $boot = function ($_EXTKEY) {
-    if (TYPO3_MODE === 'BE') {
 
-        $iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class);
+    $iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class);
 
-        // Content Elements Icons
-        $contentElementIconFilePrefix = 'EXT:' . $_EXTKEY . '/Resources/Public/Icons/ContentElements/';
-        $contentElementIcons = [
-            'content-elements-newElement' => 'customElement.svg'
-        ];
-        foreach ($contentElementIcons as $identifier => $contentElementIcon) {
-            $iconRegistry->registerIcon(
-                $identifier,
-                \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
-                ['source' => $contentElementIconFilePrefix . $contentElementIcon]
-            );
-        }
-
+    // Content Elements Icons
+    $contentElementIconFilePrefix = 'EXT:' . $_EXTKEY . '/Resources/Public/Icons/ContentElements/';
+    $contentElementIcons = [
+        'content-elements-newElement' => 'customElement.svg'
+    ];
+    foreach ($contentElementIcons as $identifier => $contentElementIcon) {
+        $iconRegistry->registerIcon(
+            $identifier,
+            \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+            ['source' => $contentElementIconFilePrefix . $contentElementIcon]
+        );
     }
 };
 
