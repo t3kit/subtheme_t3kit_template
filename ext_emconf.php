af58bd7a0e96623088f1d6f89198b8cdc03fae6a
dmh <dmytro.hrynevych@gmail.com>
2019-01-10 16:32:21 +0200

    [TASK] update dependencies, theme-t3kit v2.10.0-alpha

--- ext_emconf.php
+++ ext_emconf.php
@@ -24,7 +24,7 @@ $EM_CONF[$_EXTKEY] = array(
     'CGLcompliance_note' => '',
     'constraints' => array(
         'depends' => array(
-            'theme_t3kit' => '2.9.0-2.9.99'
+            'theme_t3kit' => '2.10.0-2.10.99'
         ),
         'conflicts' => array(
         ),
